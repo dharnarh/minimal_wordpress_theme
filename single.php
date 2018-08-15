132e3a457eda72e4cfc39ae1a31deb20911d51f7
Umar Farouq <upharouk@gmail.com>
2018-08-15 10:53:43 +0000

    Removed inc/indexCarousel from file

--- single.php
+++ single.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<?php get_template_part ( 'inc/indexCarousel' ) ?>
-
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 
         get_template_part ( 'content-single', get_post_format() );
