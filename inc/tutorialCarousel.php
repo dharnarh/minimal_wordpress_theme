43c333c755d92a73c9533254f166433a33bd9cf9
Umar Farouq Mohammed <upharouk@gmail.com>
2018-11-09 18:55:55 +0000

    Add single carousel banner for tutorial page

---
+++ inc/tutorialCarousel.php
@@ -0,0 +1,13 @@
+    <!-- Carousel section -->
+    <div class="container-fluid rmpd">
+        <div class="top-banner" style="background-image: url(<?php echo get_bloginfo( 'template_directory' );?>/assets/img/blog.png); background-position: center; background-size: cover; background-repeat: no-repeat; background-origin: initial">
+            <div class="banner flexbox text-center">
+                <div class="mx-auto align-self-center">
+                <h2 class="text-white bold">TUTORIALS</h2>
+                <p class="text-white">I write the missing articles on the internet and try to explain most articles better.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br><br>
