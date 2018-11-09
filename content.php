341319c7c6f557dd1ea0357b3467c9b8469581ad
Umar Farouq Mohammed <upharouk@gmail.com>
2018-11-09 18:49:42 +0000

    Changed element to div

--- content.php
+++ content.php
@@ -2,7 +2,7 @@
             <div class="media-body">
                 <h5 class="mt-0 bold"><a href="<?php the_permalink(); ?>" class="text-body"><?php the_title(); ?></a></h5>
                 <p class="date">Posted on <?php the_date(); ?></p>
-                <p class="text-secondary roboto"><?php the_excerpt(); ?></p>
+                <div class="text-secondary roboto"><?php the_excerpt(); ?></div>
                 <a href="<?php the_permalink(); ?>"><button class="btn btn-outline-secondary">Read post</button></a>
             </div>
         </div>
