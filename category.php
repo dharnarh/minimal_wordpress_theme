104e88c3a6c15b7895301a571f6a8afbc0fd0a60
Umar Farouq Mohammed <upharouk@gmail.com>
2018-11-09 22:06:48 +0000

    Added category template to theme

---
+++ category.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+<?php get_template_part ( 'inc/categoryCarousel' ) ?>
+
+	<!-- Blog container -->
+    <div class="container ajaxPost bg-white blog-container">
+
+        <?php
+        // Argument that defines how many posts is outputted.
+        $args = array('posts_per_page' => 5, 'paged' => 1 );
+        // Variable to call WP_Query.
+        $query = new WP_Query ( $args );
+
+        if ( $query->have_posts () ) : while( $query->have_posts() ) : $query->the_post();
+
+        	get_template_part ( 'content', get_post_format() );
+
+        endwhile; endif;
+        // Function to reset post data.
+        //wp_reset_postdata();
+        ?>
+    </div>
+
+    <br>
+
+    <!-- Load More Articles -->
+    <div class="loadp text-center">
+        <button type="button" class="btn btn-secondary loadMore">LOAD MORE <span id="sloading" class="fa fa-long-arrow-down"></span></button>
+    </div>
+
+    <br><br>
+
+<?php get_footer(); ?>
