756eec9ee4d7b940d2067986506d8995d8b82263
Umar Farouq Mohammed <upharouk@gmail.com>
2018-11-09 22:07:55 +0000

    Added function to get next ajax post

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@ function wp_styles_n_scripts () {
 	wp_enqueue_style( 'fontAwesomeCSS', 'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' );
 	wp_enqueue_style( 'mainCSS', get_template_directory_uri() . '/assets/css/main.css' );
 	wp_enqueue_style( 'styleCSS', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_script( 'slimJquery', 'https://code.jquery.com/jquery-3.3.1.slim.min.js' );
+	wp_enqueue_script( 'Jquery', 'https://code.jquery.com/jquery-3.3.1.min.js' );
 	wp_enqueue_script( 'PopperJS', 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js' );
 	wp_enqueue_script( 'bootstrapJS', 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js' );
 	wp_enqueue_script( 'mainJS', get_template_directory_uri() . '/assets/js/main.js' );
@@ -44,3 +44,25 @@ function li_a_class ( $class ) {
 	return $class;
 }
 add_filter ( 'nav_menu_link_attributes', 'li_a_class' );
+
+// Ajax function call to load more post()
+function load_posts_by_ajax_callback() {
+
+	//check_ajax_referer('load_more_posts', 'security');
+
+	$paged = $_POST['page'];
+	$args = array( 'posts_per_page' => 5, 'paged' => $paged );
+	$query = new WP_Query( $args );
+
+	if ( $query->have_posts () ) : while( $query->have_posts() ) : $query->the_post();
+
+    get_template_part ( 'content', get_post_format() );
+
+  endwhile; endif;
+  // Function to reset post data.
+	//wp_reset_postdata();
+				
+}
+
+add_action('wp_ajax_load_posts_by_ajax', 'load_posts_by_ajax_callback');
+add_action('wp_ajax_nopriv_load_posts_by_ajax', 'load_posts_by_ajax_callback');
