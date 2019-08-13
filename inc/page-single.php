f6d02c9b7651f4a16d8fbb408fd858860d1cc2ff
Umar Farouq Mohammed <upharouk@gmail.com>
2019-08-13 13:09:12 +0000

    changed background image and minor fix

--- inc/page-single.php
+++ inc/page-single.php
@@ -1,8 +1,8 @@
   <!-- Carousel section -->
   <div class="container-fluid rmpd">
-    <div class="top-banner" style="background-image: url(<?php echo get_bloginfo( 'template_directory' );?>/assets/img/blog.png); background-position: center; background-size: cover; background-repeat: no-repeat; background-origin: initial">
+    <div class="top-banner" style="background-image: url(<?php echo get_bloginfo( 'template_directory' );?>/assets/img/black.jpg); background-position: center; background-size: cover; background-repeat: no-repeat; background-origin: initial">
       <div class="postbanner flexbox text-center">
-        <h2 class="text-white mx-auto align-self-center bold"><?php the_title(); ?></h2>
+        <h2 class="text-white mx-auto align-self-center bold uf-fl"><?php the_title(); ?></h2>
       </div>
     </div>
   </div>
