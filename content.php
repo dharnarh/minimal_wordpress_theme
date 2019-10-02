27b3d75f5c1baf77578ec68a23f5b286e604d9da
Umar Farouq Mohammed <upharouk@gmail.com>
2019-10-02 02:55:12 +0000

    Added small class to date

--- content.php
+++ content.php
@@ -1,7 +1,7 @@
 <div class="media md-line">
   <div class="media-body">
     <h5 class="mt-0 bold"><a href="<?php the_permalink(); ?>" class="text-body a-hover"><?php the_title(); ?></a></h5>
-    <p class="date">Posted on <?php echo get_the_date(); ?></p>
+    <p class="date esmall">Posted on <?php echo get_the_date(); ?></p>
     <div class="text-secondary roboto"><?php the_excerpt(); ?></div>
     <a href="<?php the_permalink(); ?>"><button class="btn btn-outline-danger btn-cs">Read post</button></a>
   </div>
